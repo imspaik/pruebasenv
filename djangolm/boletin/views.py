39a62219e8486408a990a37768164a21c735b8a3
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-19 21:31:59 +0200

    video 19
    modelform en la vista (borrar boton)

--- djangolm/boletin/views.py
+++ djangolm/boletin/views.py
@@ -1,23 +1,44 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 # Create your views here.
 def inicio(request):
 	titulo = "HOLA"
 	if request.user.is_authenticated():
 		titulo = "Bienvenido %s" %(request.user)
-	form = RegForm(request.POST or None)
+	form = RegModelForm(request.POST or None)
+	context = {
+			"titulo": titulo,
+			"el_form": form,
+		}
+
 	if form.is_valid():
-		form_data = form.cleaned_data
-		abc = form_data.get("email")
-		abc2 = form_data.get("nombre")
-		obj = Registrado.objects.create(email=abc, nombre=abc2)
+		instance = form.save(commit=False)
+		nombre = form.cleaned_data.get("nombre")
+		email = form.cleaned_data.get("email")
+		if not instance.nombre:
+			instance.nombre = "PERSONA"
+		instance.save()
+
+		context = {
+			"titulo": "Gracias %s!" %(nombre)
+		}
+
+		if not nombre:
+			context = {
+			"titulo": "Gracias anonimo %s!" %(email)
+		}
+
+
+		print instance
+		print instance.timestamp
+		#form_data = form.cleaned_data
+		#abc = form_data.get("email")
+		#abc2 = form_data.get("nombre")
+		#obj = Registrado.objects.create(email=abc, nombre=abc2)
 
 		#print form_data.get("edad")
 	#print (dir(form))
-	context = {
-		"titulo": titulo,
-		"el_form": form,
-	}
+
 	return render(request, "inicio.html", context)
