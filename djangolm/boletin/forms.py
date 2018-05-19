da73ae8815944eaa803fd58ce3142138971ab85e
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-19 19:47:21 +0200

    video 17
    validaciones model form

--- djangolm/boletin/forms.py
+++ djangolm/boletin/forms.py
@@ -6,7 +6,16 @@ class RegModelForm(forms.ModelForm):
 	class Meta:
 		model = Registrado
 		fields = ["nombre", "email"]
-
+	def clean_email(self):
+		email = self.cleaned_data.get("email")
+		email_base, proveeder = email.split("@")
+		dominio, extension = proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Utiliza un email con extension .EDU")
+		return email
+	def clean_nombre (self):
+		nombre = self.cleaned_data.get("nombre")
+		return nombre	
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
