770ef4fa4d31505830ccc745e7739ef8d5820ab1
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-19 15:13:28 +0200

    video 14
    validaciones de formularios

--- djangolm/boletin/views.py
+++ djangolm/boletin/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from .forms import RegForm
 # Create your views here.
 def inicio(request):
-	form = RegForm()
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_data = form.cleaned_data
+		print form_data.get("nombre")
+		print form_data.get("edad")
+	#print (dir(form))
 	context = {
 		"el_form": form,
 	}
