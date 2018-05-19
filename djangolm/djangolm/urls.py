15e6371de8d073be69d6b9a696f3fe93d67a14ba
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-19 14:16:58 +0200

    video9
    error de plantilla

--- djangolm/djangolm/urls.py
+++ djangolm/djangolm/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from boletin import views
+# from boletin.views inicio
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.inicio, name='inicio')
 ]
